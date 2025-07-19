c843f5b3da1c0ada72d0f5890ab02618afb212c3
hejinjin <2697190181@qq.com>
2025-07-19 16:09:27 +0800

    ''

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ const a = 1;
 function fn (){
   console.log('test')
 }
+console.log('github')
+function fn2(){
+  console.log('fn2')
+}
+let obj =  {
+  brand:'1',
+  size:'2',
+  color:'red'
+}
+function sayHello(){
+  console.log('obj is',obj)
+}
